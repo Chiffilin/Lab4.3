0f936fe55462444a9e4cca5018fd99dece7fb8b0
Chiffilin <dimaaitbaev42@gmail.com>
2020-04-23 20:05:23 +0300

    Добавьте файлы проекта.

---
+++ Lab4.3/Lab4.3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+    string s;
+    size_t first_pos;
+    string first_word;
+    setlocale(LC_ALL, "RUS");
+
+    cout << "Input string: ";
+    getline(cin, s);
+
+    first_pos = s.find(' ');
+    first_word = s.substr(0, first_pos);
+
+    s.replace(s.begin(), s.begin() + first_pos, s.substr(s.rfind(' ') + 1));
+    s.replace(s.begin() + s.rfind(' ') + 1, s.end(), first_word);
+
+    cout <<"New string: "<< s << endl;
+}
